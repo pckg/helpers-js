55e548febd81b1c99e8bf10aafa325e2897c625b
Bojan Rajh <schtr4jh@schtr4jh.net>
2018-12-29 21:56:34 +0100

    Button

---
+++ vue/_input_button.vue
@@ -0,0 +1,25 @@
+<template>
+    <button type="button" class="btn btn-default">
+        <i :class="icon" v-if="icon"></i>
+        {{ text }}
+        <pckg-loader v-if="loading" :loading="loading"></pckg-loader>
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'd-input-button',
+        props: {
+            loading: {
+                type: Boolean,
+                default: false
+            },
+            text: {
+                type: String
+            },
+            icon: {
+                default: null
+            }
+        }
+    }
+</script>
