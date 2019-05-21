66f990784f04cbd5049c4230657898926ec20023
Bojan Rajh <schtr4jh@schtr4jh.net>
2019-05-21 23:01:22 +0200

    Color input, options will be extracted

--- vue/_input_color.vue
+++ vue/_input_color.vue
@@ -3,7 +3,15 @@
         <span @click.prevent="visible = !visible" class="preview" :style="{'background-color':myValue}"></span>
         <input type="text" v-model="myValue" class="model"/>
         <span class="over" v-if="visible">
-            <color-picker v-model="myValue" :width="240" :height="240"></color-picker>
+            <div class="as-table align-top">
+                <div>
+                    <color-picker v-model="myValue" :width="180" :height="180"></color-picker>
+                </div>
+                <div>
+                    <span v-for="color in colors" class="pill --square" :style="{backgroundColor: color}"
+                          :title="color"></span>
+                </div>
+            </div>
         </span>
         <div class="clearfix"></div>
     </span>
@@ -26,7 +34,6 @@
         },
         model: {
             prop: 'value',
-            //event: 'input'
         },
         data: function () {
             return {
@@ -49,5 +56,24 @@
         beforeDestroy: function () {
             $('body').off('click', this.checkCloseTrigger);
         },
+        computed: {
+            designSettings: function () {
+                return $store.state.settings.settings.design;
+            },
+            colors: function () {
+                let designSettings = this.designSettings;
+                let colors = {};
+
+                colors[designSettings.vars.colorP] = designSettings.vars.colorP;
+                colors[designSettings.vars.colorS] = designSettings.vars.colorS;
+                $.each(designSettings.scopes, function (scope, vars) {
+                    $.each(vars, function (i, color) {
+                        colors[color] = color;
+                    });
+                });
+
+                return Object.values(colors);
+            }
+        }
     }
 </script>
