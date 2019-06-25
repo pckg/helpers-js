8ae4a6f8fe22a6f1e8f9a9392b0617bc50332e39
Bojan Rajh <schtr4jh@schtr4jh.net>
2019-06-25 18:17:50 +0200

    Formatted value

--- vue/_input_numpad.vue
+++ vue/_input_numpad.vue
@@ -1,61 +1,57 @@
 <template>
-    <div class="c-input-numpad">
-        {{ formattedValue }}
+    <div class="c-input-numpad width-xs margin-0-auto">
 
-        <div class="width-sm margin-0-auto">
-
-            <div class="as-table table-fixed">
-                <div>
-                    <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(1)">1</button>
-                </div>
-                <div>
-                    <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(2)">2</button>
-                </div>
-                <div>
-                    <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(3)">2</button>
-                </div>
+        <div class="as-table table-fixed">
+            <div>
+                <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(1)">1</button>
             </div>
-
-            <div class="as-table table-fixed">
-                <div>
-                    <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(4)">4</button>
-                </div>
-                <div>
-                    <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(5)">5</button>
-                </div>
-                <div>
-                    <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(6)">6</button>
-                </div>
+            <div>
+                <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(2)">2</button>
             </div>
-
-            <div class="as-table table-fixed">
-                <div>
-                    <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(7)">7</button>
-                </div>
-                <div>
-                    <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(8)">8</button>
-                </div>
-                <div>
-                    <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(9)">9</button>
-                </div>
+            <div>
+                <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(3)">2</button>
             </div>
-
-            <div class="as-table table-fixed">
-                <div>
-                    <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="addTwo(0)">
-                        .00
-                    </button>
-                </div>
-                <div>
-                    <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(0)">0</button>
-                </div>
-                <div>
-                    <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="remove">Delete
-                    </button>
-                </div>
-            </div>
-
         </div>
+
+        <div class="as-table table-fixed">
+            <div>
+                <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(4)">4</button>
+            </div>
+            <div>
+                <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(5)">5</button>
+            </div>
+            <div>
+                <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(6)">6</button>
+            </div>
+        </div>
+
+        <div class="as-table table-fixed">
+            <div>
+                <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(7)">7</button>
+            </div>
+            <div>
+                <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(8)">8</button>
+            </div>
+            <div>
+                <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(9)">9</button>
+            </div>
+        </div>
+
+        <div class="as-table table-fixed">
+            <div>
+                <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="addTwo(0)">
+                    .00
+                </button>
+            </div>
+            <div>
+                <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="add(0)">0</button>
+            </div>
+            <div>
+                <button type="button" class="btn btn-default btn-lg btn-block" @click.prevent="remove">Delete
+                </button>
+            </div>
+        </div>
+
     </div>
 </template>
 
@@ -69,7 +65,7 @@
         },
         watch: {
             value: function (value) {
-                this.myValue = this.parseValue(value);
+                this.setValue(this.parseValue(value));
             }
         },
         model: {
@@ -81,50 +77,56 @@
             };
         },
         methods: {
+            setValue: function (value) {
+                console.log('set value', value);
+                this.myValue = value;
+                this.emitValue();
+            },
             parseValue: function (value) {
-                value = parseFloat(value);
+                value = parseFloat(value) || parseFloat(0.0);
 
                 /**
                  * Make 2 decimals.
                  * Remove ..
                  */
-                value = value.toFixed(3).split('').reverse().slice(1).reverse().filter(function(item){
-                    return typeof item === 'number';
-                });
-
-                if (value.length < 3) {
-                    value = value.reverse();
-                    while (value.length < 3) {
-                        value.push(0);
-                    }
-                    value = value.reverse();
-                }
+                let found = false;
+                value = value.toFixed(3).split('').reverse().slice(1).reverse().filter(function (item) {
+                    return item !== '.';
+                })
 
                 return value;
             },
             addTwo: function (num) {
-                this.myValue.push(num);
-                this.myValue.push(num);
+                this.add(num);
+                this.add(num);
             },
             add: function (num) {
-                if (num === 0 && !this.myValue) {
+                if (num === 0 && this.myValue.length === 0) {
                     return;
                 }
 
                 this.myValue.push(num);
+                this.emitValue();
             },
             remove: function () {
-                if (this.myValue.length === 3) {
-                    this.myValue = this.myValue.slice(0, 2).reverse().push(0).reverse();
+                if (this.myValue.length === 1) {
+                    this.myValue = [];
                     return;
                 }
 
-                this.myValue = this.myValue.slice(0, this.myValue.length - 1);
+                this.setValue(this.myValue.slice(0, this.myValue.length - 1));
+            },
+            emitValue: function () {
+                this.$emit('input', this.formattedValue)
             }
         },
         computed: {
             formattedValue: function () {
-                return this.myValue.slice(0, this.myValue.length - 2) + '.' + this.myValue.slice(this.myValue.length - 2);
+                if (this.myValue.length === 0) {
+                    return '0.00';
+                }
+
+                return this.myValue.slice(0, this.myValue.length - 2).join('') + '.' + this.myValue.slice(this.myValue.length - 2).join('');
             }
         }
     }
