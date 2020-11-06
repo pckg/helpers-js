9b028cb34bc0f77bbff5c7ae602e9bc3db4c5537
Bojan Rajh <schtr4jh@schtr4jh.net>
2020-11-06 14:44:44 +0100

    Default modal

---
+++ webpack/modal.vue
@@ -0,0 +1,36 @@
+<template>
+    <transition name="modal">
+        <div class="modal-mask">
+            <div class="modal-wrapper" @click.self="$emit('close')">
+                <div class="modal-container container --lg">
+                    <a href="#"
+                       @click.prevent="$emit('close')"
+                       class="absolute color-white"
+                       style="top: -4.8rem; right: -4.8rem; font-size: 3.6rem;"><ion-icon name="close-circle-outline"></ion-icon></a>
+                    <slot class="padding-md"></slot>
+                    <component v-if="component" :is="component" v-bind="props" class="padding-md"
+                               @close="$emit('close')"></component>
+                </div>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+export default {
+    props: {
+        component: {
+            type: String,
+            default: null
+        },
+        props: {
+            default: function () {
+                return {};
+            }
+        },
+        size: {
+            default: ''
+        }
+    }
+}
+</script>
