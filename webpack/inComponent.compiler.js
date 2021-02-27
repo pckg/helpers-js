cba5c4fe18aadd2e54bb814cd9f00d87f0dcf876
Bojan Rajh <schtr4jh@schtr4jh.net>
2021-02-27 10:55:25 +0100

    Static route compiler

---
+++ webpack/inComponent.compiler.js
@@ -0,0 +1,43 @@
+import Vue from 'vue';
+
+export function inComponentRenderer(h) {
+    if (!this.templateRender) {
+        if (this.$options.template) {
+            return this.$options.template;
+        }
+
+        return h('div', 'Loading ...');
+    }
+
+    try {
+        return this.templateRender();
+    } catch (e) {
+        console.log(e);
+        return h('div', 'Error rendering template');
+    }
+}
+
+export function inComponentCompiler(template) {
+
+    let res;
+    let b = template || '';
+    console.log('Building template', template);
+
+    try {
+        res = Vue.compile(b);
+        console.log('built', res);
+    } catch (e) {
+        console.log(e, b, res, this);
+        res = Vue.compile('<p>Error building template</p>');
+    }
+
+    this.templateRender = res.render;
+    this.$options.staticRenderFns = [];
+    this._staticTrees = [];
+    if (res.staticRenderFns) {
+        for (var i in res.staticRenderFns) {
+            this.$options.staticRenderFns.push(res.staticRenderFns[i]);
+        }
+    }
+
+}
---
+++ webpack/route.vue
@@ -0,0 +1,20 @@
+<script>
+import {
+    inComponentCompiler,
+    inComponentRenderer
+} from "./inComponent.compiler.js";
+
+export default {
+    render: function (h) {
+        inComponentRenderer.call(this, h);
+    },
+    watch: {
+        $route: {
+            immediate: true,
+            handler: function (newVal, oldVal) {
+                inComponentCompiler.call(this, newVal.meta.tags['vue:route:template']);
+            }
+        }
+    },
+}
+</script>
