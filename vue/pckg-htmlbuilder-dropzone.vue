ae57fae3cadeecac7a7fc3f28dff9821e1f02b9c
Bojan Rajh <schtr4jh@schtr4jh.net>
2021-12-12 21:00:08 +0100

    Mixins

--- vue/pckg-htmlbuilder-dropzone.vue
+++ vue/pckg-htmlbuilder-dropzone.vue
@@ -48,7 +48,7 @@
 
     export default {
         name: 'pckg-htmlbuilder-dropzone',
-        mixins: [pckgCdn],
+        mixins: [CommsHubHelpers.cdn],
         props: {
             current: {
                 type: String,
